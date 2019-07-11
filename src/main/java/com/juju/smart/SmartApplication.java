c7cab724a0be4b02bfb815965f0d425bbe556ac7
taylor <taylor_liu@kingdee.com>
2019-07-11 20:59:35 +0800

    ..

--- src/main/java/com/juju/smart/SmartApplication.java
+++ src/main/java/com/juju/smart/SmartApplication.java
@@ -1,8 +1,8 @@
 package com.juju.smart;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.juju.smart.dao"})
--- src/main/java/com/juju/smart/service/UserService.java
+++ src/main/java/com/juju/smart/service/UserService.java
@@ -1,7 +1,6 @@
 package com.juju.smart.service;
 
 import com.juju.smart.entity.User;
-import com.juju.smart.request.UserRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,4 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface UserService {
     @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
     User findById(@PathVariable("id") Long id);
-
-//    @RequestMapping(value = "user/add", method = RequestMethod.POST)
-//    Long add(@PathVariable("user")UserRequest request);
 }

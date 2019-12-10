db394791dcd9f2cca61dc39e21434c80ba4495cb
taylor <taylor_liu@kingdee.com>
2019-12-10 16:51:32 +0800

    ..

--- src/main/java/com/juju/smart/api/request/UserRequest.java
+++ src/main/java/com/juju/smart/api/request/UserRequest.java
@@ -1,12 +1,16 @@
 package com.juju.smart.api.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.juju.common.entity.JujuRequest;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.io.Serializable;
+
 @ApiModel
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UserRequest {
+public class UserRequest implements JujuRequest, Serializable {
+
     @ApiModelProperty(value = "用户id", required = true)
     private Long id;
     @ApiModelProperty(value = "用户名称", required = true)
--- src/main/java/com/juju/smart/api/service/UserService.java
+++ src/main/java/com/juju/smart/api/service/UserService.java
@@ -14,4 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface UserService {
     @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
     User findById(@PathVariable("id") Long id);
+
+
 }
